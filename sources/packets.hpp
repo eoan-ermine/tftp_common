6ab38d90f7df36672a644bda4823a491afb043af
PatriotRossii <patriotrossii2019@mail.ru>
2023-05-22 17:58:17 +0300

    Implement user-friendly constructors for packets

--- sources/packets.hpp
+++ sources/packets.hpp
@@ -2,6 +2,7 @@
 
 #include <arpa/inet.h>
 
+#include <string_view>
 #include <cstdint>
 #include <vector>
 #include <string>
@@ -13,6 +14,8 @@ namespace packets {
 class request {
 public:
 	request();
+	request(std::uint16_t type_, std::string_view filename_, std::string_view mode_)
+		: type(type_), filename(filename_.begin(), filename_.end() + 1), mode(mode_.begin(), mode_.end() + 1) { }
 	~request();
 
 	void serialize(std::vector<std::uint8_t>& buf);
@@ -25,6 +28,10 @@ public:
 class data {
 public:
 	data();
+	data(std::uint16_t block_, const std::vector<std::uint8_t>& buffer_)
+		: block(block_), data_(buffer_.begin(), buffer_.end()) { }
+	data(std::uint16_t block_, std::vector<std::uint8_t>&& buffer_)
+		: block(block_), data_(std::move(buffer_)) { }
 	~data();
 
 	void serialize(std::vector<std::uint8_t>& buf);
@@ -37,6 +44,8 @@ public:
 class acknowledgment {
 public:
 	acknowledgment();
+	acknowledgment(std::uint16_t block_)
+		: block(block_) { }
 	~acknowledgment();
 
 	void serialize(std::vector<std::uint8_t>& buf);
@@ -48,6 +57,8 @@ public:
 class error {
 public:
 	error();
+	error(std::uint16_t error_code_, std::string_view error_message_)
+		: error_code(error_code_), error_message(error_message_.begin(), error_message_.end() + 1) { }
 	~error();
 
 	void serialize(std::vector<std::uint8_t>& buf);
