fdbd95727ad0fc36c46596686a532fc5539e6227
PatriotRossii <patriotrossii2019@mail.ru>
2023-06-09 21:10:26 +0300

    Add .clang-format, reformat all files with this policy

--- tftp_common/packets.hpp
+++ tftp_common/packets.hpp
@@ -2,114 +2,120 @@
 
 #include <arpa/inet.h>
 
-#include <string_view>
 #include <cstdint>
-#include <vector>
 #include <string>
+#include <string_view>
+#include <vector>
 
 namespace tftp_common {
 
 namespace packets {
 
-enum type: std::uint16_t {
-	read_request = 0x01, write_request = 0x02, data_packet = 0x03, acknowledgment_packet = 0x04, error_packet = 0x05
+enum type : std::uint16_t {
+    read_request = 0x01,
+    write_request = 0x02,
+    data_packet = 0x03,
+    acknowledgment_packet = 0x04,
+    error_packet = 0x05
 };
 
 class request {
-public:
-	request() { }
-	request(type type, std::string_view filename, std::string_view mode)
-		: type_(type), filename(filename.begin(), filename.end() + 1), mode(mode.begin(), mode.end() + 1) { }
-	~request() { }
+  public:
+    request() {}
+    request(type type, std::string_view filename, std::string_view mode)
+        : type_(type), filename(filename.begin(), filename.end() + 1), mode(mode.begin(), mode.end() + 1) {}
+    ~request() {}
 
-	std::size_t serialize(std::vector<std::uint8_t>& buf) {
-		buf.push_back(htons(type_) >> 8);
-		buf.push_back(htons(type_) >> 0);
-		for (auto byte: filename) {
-			buf.push_back(byte);
-		}
-		for (auto byte: mode) {
-			buf.push_back(byte);
-		}
+    std::size_t serialize(std::vector<std::uint8_t> &buf) {
+        buf.push_back(htons(type_) >> 8);
+        buf.push_back(htons(type_) >> 0);
+        for (auto byte : filename) {
+            buf.push_back(byte);
+        }
+        for (auto byte : mode) {
+            buf.push_back(byte);
+        }
 
-		return sizeof(type_) + filename.size() + mode.size();
-	}
-public:
-	std::uint16_t type_;
-	std::vector<std::uint8_t> filename;
-	std::vector<std::uint8_t> mode;
+        return sizeof(type_) + filename.size() + mode.size();
+    }
+
+  public:
+    std::uint16_t type_;
+    std::vector<std::uint8_t> filename;
+    std::vector<std::uint8_t> mode;
 };
 
 class data {
-public:
-	data() { }
-	data(std::uint16_t block, const std::vector<std::uint8_t>& buffer)
-		: block(block), data_(buffer.begin(), buffer.end()) { }
-	data(std::uint16_t block, std::vector<std::uint8_t>&& buffer)
-		: block(block), data_(std::move(buffer)) { }
-	~data() { }
+  public:
+    data() {}
+    data(std::uint16_t block, const std::vector<std::uint8_t> &buffer)
+        : block(block), data_(buffer.begin(), buffer.end()) {}
+    data(std::uint16_t block, std::vector<std::uint8_t> &&buffer) : block(block), data_(std::move(buffer)) {}
+    ~data() {}
 
-	std::size_t serialize(std::vector<std::uint8_t>& buf) {
-		buf.push_back(htons(type) >> 8);
-		buf.push_back(htons(type) >> 0);
-		buf.push_back(htons(block) >> 8);
-		buf.push_back(htons(block) >> 0);
-		for (auto byte: data_) {
-			buf.push_back(byte);
-		}
+    std::size_t serialize(std::vector<std::uint8_t> &buf) {
+        buf.push_back(htons(type) >> 8);
+        buf.push_back(htons(type) >> 0);
+        buf.push_back(htons(block) >> 8);
+        buf.push_back(htons(block) >> 0);
+        for (auto byte : data_) {
+            buf.push_back(byte);
+        }
 
-		return sizeof(type) + sizeof(block) + data_.size();
-	}
-public:
-	std::uint16_t type = type::data_packet;
-	std::uint16_t block;
-	std::vector<std::uint8_t> data_;
+        return sizeof(type) + sizeof(block) + data_.size();
+    }
+
+  public:
+    std::uint16_t type = type::data_packet;
+    std::uint16_t block;
+    std::vector<std::uint8_t> data_;
 };
 
 class acknowledgment {
-public:
-	acknowledgment() { }
-	acknowledgment(std::uint16_t block)
-		: block(block) { }
-	~acknowledgment() { }
+  public:
+    acknowledgment() {}
+    acknowledgment(std::uint16_t block) : block(block) {}
+    ~acknowledgment() {}
 
-	std::size_t serialize(std::vector<std::uint8_t>& buf) { 
-		buf.push_back(htons(type) >> 8);
-		buf.push_back(htons(type) >> 0);
-		buf.push_back(htons(block) >> 8);
-		buf.push_back(htons(block) >> 0);
+    std::size_t serialize(std::vector<std::uint8_t> &buf) {
+        buf.push_back(htons(type) >> 8);
+        buf.push_back(htons(type) >> 0);
+        buf.push_back(htons(block) >> 8);
+        buf.push_back(htons(block) >> 0);
 
-		return sizeof(type) + sizeof(block);
-	}
-public:
-	std::uint16_t type = type::acknowledgment_packet;
-	std::uint16_t block;
+        return sizeof(type) + sizeof(block);
+    }
+
+  public:
+    std::uint16_t type = type::acknowledgment_packet;
+    std::uint16_t block;
 };
 
 class error {
-public:
-	error() { }
-	error(std::uint16_t error_code, std::string_view error_message)
-		: error_code(error_code), error_message(error_message.begin(), error_message.end() + 1) { }
-	~error() { }
+  public:
+    error() {}
+    error(std::uint16_t error_code, std::string_view error_message)
+        : error_code(error_code), error_message(error_message.begin(), error_message.end() + 1) {}
+    ~error() {}
 
-	std::size_t serialize(std::vector<std::uint8_t>& buf) { 
-		buf.push_back(htons(type) >> 8);
-		buf.push_back(htons(type) >> 0);
-		buf.push_back(htons(error_code) >> 8);
-		buf.push_back(htons(error_code) >> 0);
-		for (auto byte: error_message) {
-			buf.push_back(byte);
-		}
+    std::size_t serialize(std::vector<std::uint8_t> &buf) {
+        buf.push_back(htons(type) >> 8);
+        buf.push_back(htons(type) >> 0);
+        buf.push_back(htons(error_code) >> 8);
+        buf.push_back(htons(error_code) >> 0);
+        for (auto byte : error_message) {
+            buf.push_back(byte);
+        }
 
-		return sizeof(type) + sizeof(error_code) + error_message.size();
-	}
-public:
-	std::uint16_t type = type::error_packet;
-	std::uint16_t error_code;
-	std::vector<std::uint8_t> error_message;
+        return sizeof(type) + sizeof(error_code) + error_message.size();
+    }
+
+  public:
+    std::uint16_t type = type::error_packet;
+    std::uint16_t error_code;
+    std::vector<std::uint8_t> error_message;
 };
 
-}
+} // namespace packets
 
-}
+} // namespace tftp_common
--- tftp_common/parser.hpp
+++ tftp_common/parser.hpp
@@ -4,176 +4,176 @@
 
 namespace tftp_common {
 
-class parser
-{
-public:
-	bool parse(std::uint8_t* buffer, std::size_t len, std::size_t& bytes_read, packets::request& packet) {
-		bytes_read = 0;
-		for (std::size_t i = 0; i != len; ++i) {
-			const auto byte = buffer[i];
-			bytes_read++;
+class parser {
+  public:
+    bool parse(std::uint8_t *buffer, std::size_t len, std::size_t &bytes_read, packets::request &packet) {
+        bytes_read = 0;
+        for (std::size_t i = 0; i != len; ++i) {
+            const auto byte = buffer[i];
+            bytes_read++;
 
-			switch (step_) {
-			// Opcode (2 bytes)
-			case 0:
-				packet.type_ = std::uint16_t(byte) << 8;
-				step_++;
-				break;
-			case 1:
-				packet.type_ |= std::uint16_t(byte) << 0;
-				packet.type_ = ntohs(packet.type_);
-				if (packet.type_ != packets::type::read_request && packet.type_ != packets::type::write_request) {
-					step_ = 0;
-					continue;
-				}
-				step_++;
-				break;
-			// Filename
-			case 2:
-				packet.filename.push_back(byte);
+            switch (step_) {
+            // Opcode (2 bytes)
+            case 0:
+                packet.type_ = std::uint16_t(byte) << 8;
+                step_++;
+                break;
+            case 1:
+                packet.type_ |= std::uint16_t(byte) << 0;
+                packet.type_ = ntohs(packet.type_);
+                if (packet.type_ != packets::type::read_request && packet.type_ != packets::type::write_request) {
+                    step_ = 0;
+                    continue;
+                }
+                step_++;
+                break;
+            // Filename
+            case 2:
+                packet.filename.push_back(byte);
 
-				if (!byte)
-					step_++;
-				break;
-			// Mode
-			case 3:
-				packet.mode.push_back(byte);
+                if (!byte)
+                    step_++;
+                break;
+            // Mode
+            case 3:
+                packet.mode.push_back(byte);
 
-				if (!byte) {
-					step_ = 0;
-					return true;
-				}
-				break;
-			}
-		}
-		return false;
-	}
-	bool parse(std::uint8_t* buffer, std::size_t len, std::size_t& bytes_read, packets::data& packet) {
-		bytes_read = 0;
-		for (std::size_t i = 0; i != len; ++i) {
-			const auto byte = buffer[i];
-			bytes_read++;
+                if (!byte) {
+                    step_ = 0;
+                    return true;
+                }
+                break;
+            }
+        }
+        return false;
+    }
+    bool parse(std::uint8_t *buffer, std::size_t len, std::size_t &bytes_read, packets::data &packet) {
+        bytes_read = 0;
+        for (std::size_t i = 0; i != len; ++i) {
+            const auto byte = buffer[i];
+            bytes_read++;
 
-			switch (step_) {
-			// Opcode (2 bytes)
-			case 0:
-				packet.type = std::uint16_t(byte) << 8;
-				step_++;
-				break;
-			case 1:
-				packet.type |= std::uint16_t(byte) << 0;
-				packet.type = ntohs(packet.type);
-				if (packet.type != packets::type::data_packet) {
-					step_ = 0;
-					continue;
-				}
-				step_++;
-				break;
-			// Block # (2 bytes)
-			case 2:
-				packet.block = std::uint16_t(byte) << 8;
-				step_++;
-				break;
-			case 3:
-				packet.block |= std::uint16_t(byte) << 0;
-				packet.block = ntohs(packet.block);
-				step_++;
-				break;
-			// buffer
-			case 4:
-				packet.data_.push_back(byte);
+            switch (step_) {
+            // Opcode (2 bytes)
+            case 0:
+                packet.type = std::uint16_t(byte) << 8;
+                step_++;
+                break;
+            case 1:
+                packet.type |= std::uint16_t(byte) << 0;
+                packet.type = ntohs(packet.type);
+                if (packet.type != packets::type::data_packet) {
+                    step_ = 0;
+                    continue;
+                }
+                step_++;
+                break;
+            // Block # (2 bytes)
+            case 2:
+                packet.block = std::uint16_t(byte) << 8;
+                step_++;
+                break;
+            case 3:
+                packet.block |= std::uint16_t(byte) << 0;
+                packet.block = ntohs(packet.block);
+                step_++;
+                break;
+            // buffer
+            case 4:
+                packet.data_.push_back(byte);
 
-				if (i == len - 1) {
-					step_ = 0;
-					return true;
-				}
+                if (i == len - 1) {
+                    step_ = 0;
+                    return true;
+                }
 
-				break;
-			}
-		}
-		return false;
-	}
-	bool parse(std::uint8_t* buffer, std::size_t len, std::size_t& bytes_read, packets::acknowledgment& packet) {
-		bytes_read = 0;
-		for (std::size_t i = 0; i != len; ++i) {
-			const auto byte = buffer[i];
-			bytes_read++;
+                break;
+            }
+        }
+        return false;
+    }
+    bool parse(std::uint8_t *buffer, std::size_t len, std::size_t &bytes_read, packets::acknowledgment &packet) {
+        bytes_read = 0;
+        for (std::size_t i = 0; i != len; ++i) {
+            const auto byte = buffer[i];
+            bytes_read++;
 
-			switch (step_) {
-			// Opcode (2 bytes)
-			case 0:
-				packet.type = std::uint16_t(byte) << 8;
-				step_++;
-				break;
-			case 1:
-				packet.type |= std::uint16_t(byte) << 0;
-				packet.type = ntohs(packet.type);
-				if (packet.type != packets::type::acknowledgment_packet) {
-					step_ = 0;
-					continue;
-				}
-				step_++;
-				break;
-			// Block # (2 bytes)
-			case 2:
-				packet.block = std::uint16_t(byte) << 8;
-				step_++;
-				break;
-			case 3:
-				packet.block |= std::uint16_t(byte) << 0;
-				packet.block = ntohs(packet.block);
-				step_ = 0;
-				return true;
-			}
-		}
-		return false;
-	}
-	bool parse(std::uint8_t* buffer, std::size_t len, std::size_t& bytes_read, packets::error& packet) {
-		bytes_read = 0;
-		for (std::size_t i = 0; i != len; ++i) {
-			const auto byte = buffer[i];
-			bytes_read++;
+            switch (step_) {
+            // Opcode (2 bytes)
+            case 0:
+                packet.type = std::uint16_t(byte) << 8;
+                step_++;
+                break;
+            case 1:
+                packet.type |= std::uint16_t(byte) << 0;
+                packet.type = ntohs(packet.type);
+                if (packet.type != packets::type::acknowledgment_packet) {
+                    step_ = 0;
+                    continue;
+                }
+                step_++;
+                break;
+            // Block # (2 bytes)
+            case 2:
+                packet.block = std::uint16_t(byte) << 8;
+                step_++;
+                break;
+            case 3:
+                packet.block |= std::uint16_t(byte) << 0;
+                packet.block = ntohs(packet.block);
+                step_ = 0;
+                return true;
+            }
+        }
+        return false;
+    }
+    bool parse(std::uint8_t *buffer, std::size_t len, std::size_t &bytes_read, packets::error &packet) {
+        bytes_read = 0;
+        for (std::size_t i = 0; i != len; ++i) {
+            const auto byte = buffer[i];
+            bytes_read++;
 
-			switch (step_) {
-			// Opcode (2 bytes)
-			case 0:
-				packet.type = std::uint16_t(byte) << 8;
-				step_++;
-				break;
-			case 1:
-				packet.type |= std::uint16_t(byte) << 0;
-				packet.type = ntohs(packet.type);
-				if (packet.type != packets::type::error_packet) {
-					step_ = 0;
-					continue;
-				}
-				step_++;
-				break;
-			// ErrorCode (2 bytes)
-			case 2:
-				packet.error_code = std::uint16_t(byte) << 8;
-				step_++;
-				break;
-			case 3:
-				packet.error_code |= std::uint16_t(byte) << 0;
-				packet.error_code = ntohs(packet.type);
-				step_++;
-				break;
-			// ErrorMessage
-			case 4:
-				packet.error_message.push_back(byte);
+            switch (step_) {
+            // Opcode (2 bytes)
+            case 0:
+                packet.type = std::uint16_t(byte) << 8;
+                step_++;
+                break;
+            case 1:
+                packet.type |= std::uint16_t(byte) << 0;
+                packet.type = ntohs(packet.type);
+                if (packet.type != packets::type::error_packet) {
+                    step_ = 0;
+                    continue;
+                }
+                step_++;
+                break;
+            // ErrorCode (2 bytes)
+            case 2:
+                packet.error_code = std::uint16_t(byte) << 8;
+                step_++;
+                break;
+            case 3:
+                packet.error_code |= std::uint16_t(byte) << 0;
+                packet.error_code = ntohs(packet.type);
+                step_++;
+                break;
+            // ErrorMessage
+            case 4:
+                packet.error_message.push_back(byte);
 
-				if (!byte) {
-					step_ = 0;
-					return true;
-				}
-				break;
-			}
-		}
-		return false;
-	}
-private:
-	int step_ = 0;
+                if (!byte) {
+                    step_ = 0;
+                    return true;
+                }
+                break;
+            }
+        }
+        return false;
+    }
+
+  private:
+    int step_ = 0;
 };
 
-}
+} // namespace tftp_common
