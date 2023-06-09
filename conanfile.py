3c04ec9eb612ee1a8c7dd3e7b1f0968ac942d195
PatriotRossii <patriotrossii2019@mail.ru>
2023-06-09 23:58:44 +0300

    Add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,18 @@
+import os
+
+from conan import ConanFile
+from conan.tools.files import copy
+
+class TFTPCommonConan(ConanFile):
+	name = "tftp_common"
+	version = "1.0.0"
+	url = "https://github.com/eoan-ermine/tftp_common"
+	description = " A simple header-only Trivial File Transfer Protocol packets parsing and serializing library"
+	license = "Unlicense"
+	exports = ["LICENSE"]
+	exports_sources = "tftp_common/*.hpp"
+	no_copy_source = True
+
+	def package(self):
+		copy(self, "UNLICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+		copy(self, "tftp_common/*.hpp", src=self.source_folder, dst=os.path.join(self.package_folder, "include"))
